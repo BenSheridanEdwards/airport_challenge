c367a1c38bd745c0700949d452bc80a99b8f4b69
Ben <bensheridanedwards@gmail.com>
2024-06-27 19:28:26 +0100

    separate into ruby and js

--- src/Weather/Weather.tsx
+++
@@ -1,8 +0,0 @@
-// Weather.tsx
-
-const FORECASTS: string[] = ['stormy', 'sunny', 'sunny', 'sunny'];
-
-export const isStormy = (): boolean => {
-  const forecast = FORECASTS[Math.floor(Math.random() * FORECASTS.length)];
-  return forecast === 'stormy';
-};
