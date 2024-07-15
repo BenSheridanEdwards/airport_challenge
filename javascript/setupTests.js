7c9571e27d68e5f005051f805b89b39116e82f24
Ubuntu <ubuntu@ip-10-240-131-207.us-west-2.compute.internal>
2024-07-15 23:28:34 +0000

    Update package-lock and setupTests configuration

--- javascript/setupTests.js
+++ javascript/setupTests.js
@@ -3,7 +3,10 @@ import '@testing-library/jest-dom/extend-expect';
 import { ToastContainer } from 'react-toastify';
 
 // Mock react-toastify
-jest.mock('react-toastify', () => ({
-  toast: jest.fn(),
-  ToastContainer: jest.fn(() => null),
-}));
+jest.mock('react-toastify', () => {
+  console.log('Mocking react-toastify');
+  return {
+    toast: jest.fn(),
+    ToastContainer: jest.fn(() => null),
+  };
+});
